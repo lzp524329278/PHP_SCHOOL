e50a93be1bbd6cbbb1c24f58871309881827d372
lizhaopeng <524329278@qq.com>
2016-04-30 12:28:41 +0800

    注册页提交注册功能实现，未经过表单验证，及防刷新设置

--- code/Application/Home/Conf/config.php
+++ code/Application/Home/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+        'DB_TYPE'   => 'mysql', // 数据库类型
+        'DB_HOST'   => 'localhost', // 服务器地址
+        'DB_NAME'   => 'legoushop', // 数据库名
+        'DB_USER'   => 'lzp', // 用户名
+        'DB_PWD'    => 'LZP@@@718918', // 密码
+        'DB_PORT'   => 3306, // 端口
+        'DB_PREFIX' => 'legoushop_', // 数据库表前缀 
 );
--- code/index.php
+++ code/index.php
@@ -11,18 +11,19 @@
 
 // 应用入口文件
 
-// �?测PHP环境
+// �?测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-// �?启调试模�? 建议�?发阶段开�? 部署阶段注释或�?�设为false
+// �?启调试模�? 建议�?发阶段开�? 部署阶段注释或�?�设为false
 define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
 
+define('INDEX_PATH','/PHP_SCHOOL/code/index.php');
 
-define('HOME_PUBLIC','/software/PHP_SCHOOL/code/Application/Home/Public/');
+define('HOME_PUBLIC','/PHP_SCHOOL/code/Application/Home/Public/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
-// 亲^_^ 后面不需要任何代码了 就是如此�?�?
+// 亲^_^ 后面不需要任何代码了 就是如此�?�?
